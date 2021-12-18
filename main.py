e8be6ccf5452c9e37a53291aa0b6d202068721d8
mrtb2000 <mrtbeheshti@gmail.com>
2021-12-18 13:13:24 +0330

    main.py reads data from file and creates a Table Object

---
+++ main.py
@@ -0,0 +1,15 @@
+from Table import Table as T
+
+src_dir = 'input1_method2.txt'
+f = open(src_dir)
+x, y = map(int, f.readline().split())
+rows_positive = list(map(int, f.readline().split()))
+rows_negative = list(map(int, f.readline().split()))
+columns_positive = list(map(int, f.readline().split()))
+columns_negative = list(map(int, f.readline().split()))
+table = []
+while (row := f.readline()):
+    table.append(list(map(int, row.split())))
+f.close()
+table = T(rows_negative, columns_negative, rows_positive, columns_positive,
+          table)
