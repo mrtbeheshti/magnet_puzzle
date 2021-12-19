db0fdc6c85d807e6494099cd76433b40196553c8
mrtb2000 <mrtbeheshti@gmail.com>
2021-12-19 10:46:29 +0330

    Now, Table class has a string table that shows position of magnets and free spaces

--- Table.py
+++ Table.py
@@ -25,27 +25,24 @@ class Table:
                         self.colum_pairs[j].append((i, i + 1))
                         table[i][j], table[i + 1][j] = 0, 0
         self.magnets = []
+        self.table = []
+        for i in range(len(self.rows_negative)):
+            self.table.append('0' * len(self.columns_negative))
 
     def add_magnet(self, p_x, p_y, n_x, n_y):
-        if self.rows_negative[n_x] > 0 and self.columns_negative[
-                n_y] > 0 and self.rows_positive[
-                    p_x] > 0 and self.columns_positive[p_y]:
-            new_m = Magnet(p_x, p_y, n_x, n_y)
-            self.magnets.append(new_m)
-            if p_x == n_x:
-                self.row_pairs[p_x].remove((p_y, n_y) if p_y < n_y else (n_y,
-                                                                         p_y))
-            else:
-                self.colum_pairs[p_y].remove((p_x,
-                                              n_x) if p_x < n_x else (n_x,
-                                                                      p_x))
-            self.rows_negative[n_x] -= 1
-            self.columns_negative[n_y] -= 1
-            self.rows_positive[p_x] -= 1
-            self.columns_positive[p_y] -= 1
-            return 1
+        new_m = Magnet(p_x, p_y, n_x, n_y)
+        self.magnets.append(new_m)
+        self.add_to_table(new_m)
+        if p_x == n_x:
+            self.row_pairs[p_x].remove((p_y, n_y) if p_y < n_y else (n_y, p_y))
         else:
-            return 0
+            self.colum_pairs[p_y].remove((p_x, n_x) if p_x < n_x else (n_x,
+                                                                       p_x))
+        self.rows_negative[n_x] -= 1
+        self.columns_negative[n_y] -= 1
+        self.rows_positive[p_x] -= 1
+        self.columns_positive[p_y] -= 1
+        return 1
 
     def remove_magnet(self, p_x, p_y, n_x, n_y):
         for magnet in self.magnets:
@@ -67,3 +64,51 @@ class Table:
                 self.columns_positive[p_y] += 1
                 return 1
         return 0
+
+    def is_consistent(self):
+        for i in range(len(self.rows_negative)):
+            if not (self.rows_negative[i] > 0) or not (self.rows_positive[i] >
+                                                       0):
+                return False
+        for i in range(len(self.columns_negative)):
+            if not (self.columns_negative[i] >
+                    0) or not (self.columns_positive[i] > 0):
+                return False
+        for i in range(len(self.table)):
+            for j in range(len(self.table[i])):
+                if self.table[i][j] == self.table[
+                        i - 1][j] or self.table[i][j] == self.table[
+                            i + 1][j] or self.table[i][j] == self.table[i][
+                                j - 1] or self.table[i][j] == self.table[i][j +
+                                                                            1]:
+                    return False
+        return True
+
+    def is_complete(self):
+        for i in range(len(self.rows_positive)):
+            if self.rows_positive[i] or self.rows_negative[i]:
+                return False
+        for i in range(len(self.rows_positive)):
+            if self.columns_positive[i] or self.columns_negative[i]:
+                return False
+        return True
+
+    def add_to_table(self, magnet):
+        positive_row = self.table[magnet.positive_pos['x']]
+        negative_row = self.table[magnet.negative_pos['x']]
+        positive_row = positive_row[:magnet.
+                                    positive_pos['y']] + '+' + positive_row[
+                                        magnet.positive_pos['y'] + 1:]
+        negative_row = negative_row[:magnet.
+                                    negative_pos['y']] + '-' + negative_row[
+                                        magnet.negative_pos['y'] + 1:]
+
+    def remove_from_table(self, magnet):
+        positive_row = self.table[magnet.positive_pos['x']]
+        negative_row = self.table[magnet.negative_pos['x']]
+        positive_row = positive_row[:magnet.
+                                    positive_pos['y']] + '0' + positive_row[
+                                        magnet.positive_pos['y'] + 1:]
+        negative_row = negative_row[:magnet.
+                                    negative_pos['y']] + '0' + negative_row[
+                                        magnet.negative_pos['y'] + 1:]
