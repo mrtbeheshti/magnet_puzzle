bc2035de81ea68edca86b853ed2669a10ece668a
mrtb2000 <mrtbeheshti@gmail.com>
2021-12-30 14:14:16 +0330

    MRV and LCV heuristics added to the project

--- Table.py
+++ Table.py
@@ -1,6 +1,10 @@
 import time
 from Magnet import Magnet
 
+plus = '+'
+minus = '-'
+empty = '0'
+
 
 class Table:
     def __init__(self, rows_negative, columns_negative, rows_positive,
@@ -28,7 +32,7 @@ class Table:
         self.magnets = []
         self.table = []
         for i in range(len(self.rows_negative)):
-            self.table.append('0' * len(self.columns_negative))
+            self.table.append(empty * len(self.columns_negative))
 
     def add_magnet(self, magnet):
         p_x, p_y = magnet.positive_pos['x'], magnet.positive_pos['y']
@@ -64,40 +68,37 @@ class Table:
                      and self.table[i][j] == self.table[i + 1][j]) or
                     (j > 0 and self.table[i][j] == self.table[i][j - 1]) or
                     (j < len(self.table[i]) - 1 and self.table[i][j]
-                     == self.table[i][j + 1])) and (self.table[i][j] != '0'):
+                     == self.table[i][j + 1])) and (self.table[i][j] != empty):
                     return False
         return True
 
     def is_complete(self):
         for i in range(len(self.rows_positive)):
-            if self.rows_positive[i] or self.rows_negative[i]:
-                return False
-        for i in range(len(self.rows_positive)):
-            if self.columns_positive[i] or self.columns_negative[i]:
+            if self.rows_positive[i]:
                 return False
         return True
 
     def add_to_table(self, magnet):
         positive_row = self.table[magnet.positive_pos['x']]
         positive_row = positive_row[:magnet.
-                                    positive_pos['y']] + '+' + positive_row[
+                                    positive_pos['y']] + plus + positive_row[
                                         magnet.positive_pos['y'] + 1:]
         self.table[magnet.positive_pos['x']] = positive_row
         negative_row = self.table[magnet.negative_pos['x']]
         negative_row = negative_row[:magnet.
-                                    negative_pos['y']] + '-' + negative_row[
+                                    negative_pos['y']] + minus + negative_row[
                                         magnet.negative_pos['y'] + 1:]
         self.table[magnet.negative_pos['x']] = negative_row
 
     def remove_from_table(self, magnet):
         positive_row = self.table[magnet.positive_pos['x']]
         positive_row = positive_row[:magnet.
-                                    positive_pos['y']] + '0' + positive_row[
+                                    positive_pos['y']] + empty + positive_row[
                                         magnet.positive_pos['y'] + 1:]
         self.table[magnet.positive_pos['x']] = positive_row
         negative_row = self.table[magnet.negative_pos['x']]
         negative_row = negative_row[:magnet.
-                                    negative_pos['y']] + '0' + negative_row[
+                                    negative_pos['y']] + empty + negative_row[
                                         magnet.negative_pos['y'] + 1:]
         self.table[magnet.negative_pos['x']] = negative_row
 
@@ -119,7 +120,7 @@ class Table:
 
         for i in range(len(self.column_pairs)):
             for pair in self.column_pairs[i]:
-                if self.is_empty_pair((pair[0],i), (pair[1],i)):
+                if self.is_empty_pair((pair[0], i), (pair[1], i)):
                     magnet = Magnet(pair[0], i, pair[1], i)
                     self.add_magnet(magnet)
                     if self.is_consistent():
@@ -135,23 +136,57 @@ class Table:
 
     def is_empty_pair(self, cell1, cell2):
         if self.table[cell1[0]][cell1[1]] != '0' and self.table[cell2[0]][
-                cell2[1]] != '0':
+                cell2[1]] != empty:
             return False
         return True
 
-    def print_table(self):
-        for row in self.table:
-            print(f"{row}")
+    def get_actions_sorted(self, possible_magnets):
+        if (len(possible_magnets) > 1):
+            MRV_list = sorted(
+                possible_magnets,
+                key=lambda x: self.rows_positive[x.positive_pos[
+                    'x']] + self.columns_positive[x.positive_pos[
+                        'y']] + self.rows_negative[x.negative_pos[
+                            'x']] + self.columns_negative[x.negative_pos['y']])
+            last_magnet = MRV_list[-1]
+            LCV = []
+            for i in range(
+                    4, self.rows_positive[last_magnet.positive_pos['x']] +
+                    self.columns_positive[last_magnet.positive_pos['y']] +
+                    self.rows_negative[last_magnet.negative_pos['x']] +
+                    self.columns_negative[last_magnet.negative_pos['y']] + 1):
+                l = list(
+                    filter(
+                        lambda x: self.rows_positive[x.positive_pos[
+                            'x']] + self.columns_positive[x.positive_pos['y']]
+                        + self.rows_negative[x.negative_pos['x']] + self.
+                        columns_negative[x.negative_pos['y']] == i, MRV_list))
+                l.sort(key=self.get_cv, reverse=True)
+                LCV += l
+            return LCV
+        else:
+            return possible_magnets
 
-    def solve(self, step):
+    def get_cv(self, magnet):
+        self.add_magnet(magnet)
+        possibles = len(self.get_possible_magnets())
+        self.remove_magnet(magnet)
+        return possibles
+
+    def solve(self, depth):
         if self.is_complete() and self.is_consistent():
             return True
         possible_magnets = self.get_possible_magnets()
-        for magnet in possible_magnets:
+        MRV_Asc = self.get_actions_sorted(possible_magnets)
+        for magnet in MRV_Asc:
             self.add_magnet(magnet)
-            solved = self.solve(step + 1)
+            solved = self.solve(depth + 1)
             if not solved:
                 self.remove_magnet(magnet)
             else:
                 return True
         return False
+
+    def print_table(self):
+        for row in self.table:
+            print(f"{row}")
